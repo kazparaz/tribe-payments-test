fbff6597f4bcfa2a705554fc9e61e438cd916187
Kasparas <kasparas@searchnode.com>
2021-09-15 13:20:42 +0300

    Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
+import { colors } from '../styles/variables'
 
 export const Header = (): JSX.Element => {
-  return <header>Crypto single-page application</header>
+  return (
+    <>
+      <header>
+        <h1>Crypto single-page application</h1>
+      </header>
+
+      <style jsx>{`
+        header {
+          padding: 20px;
+          text-align: center;
+          border-bottom: 10px solid ${colors.purple};
+        }
+      `}</style>
+    </>
+  )
 }
